6da22c54a63989eef5ac19c33e55400d1e61e976
idominguez <israel.dominguez.garcia@gmail.com>
2018-06-26 10:29:14 +0200

    defer false

--- src/Providers/AdminServiceProvider.php
+++ src/Providers/AdminServiceProvider.php
@@ -11,7 +11,7 @@ class AdminServiceProvider extends \Illuminate\Support\ServiceProvider
      *
      * @var bool
      */
-    protected $defer = true;
+    protected $defer = false;
 
     /**
      * Bootstrap the application services.
