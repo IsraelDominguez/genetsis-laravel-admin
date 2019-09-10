90fe1fe22000cf3aa3d7e041fcc8b5cbfd2f1d67
idominguez <israel.dominguez.garcia@gmail.com>
2019-09-10 10:50:37 +0200

    change user manage permissions

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -5,7 +5,7 @@ Route::group(['middleware' => ['web']], function () {
         Route::group(['namespace' => 'Genetsis\Admin\Controllers'], function () {
             Route::get('home', 'HomeController@index')->name('adminhome');
 
-            Route::group(['middleware' => ['role:SuperAdmin']], function () {
+            Route::group(['middleware' => ['permission:manage_users']], function () {
                 Route::get('users/get', 'UserController@get')->name('users.api');
                 Route::resource('users', 'UserController', [
                     'only' => ['index', 'edit', 'create', 'store', 'update', 'destroy', 'show'],
