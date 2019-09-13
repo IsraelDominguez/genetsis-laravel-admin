7864976e7cdf905529e0277f0c443dba8cbdfd8d
idominguez <israel.dominguez.garcia@gmail.com>
2019-09-13 09:54:06 +0200

    alpaca text not work

---
+++ resources/assets/js/alpaca.js
@@ -0,0 +1,34 @@
+var schema_left = {
+    "properties": {
+        "promo_title": {
+            "type": "string",
+            "title": "Title"
+        },
+        "promo_text": {
+            "type": "string",
+            "title": "Text"
+        },
+        "promo_image": {
+            "type": "string",
+            "format": "uri"
+        }
+    }
+};
+
+var schema_right = {
+    "properties": {
+        "promo_title": {
+            "type": "string",
+            "title": "Title"
+        },
+        "promo_text": {
+            "type": "string",
+            "title": "Text"
+        },
+        "promo_image": {
+            "type": "string"
+        }
+    }
+};
+
+
