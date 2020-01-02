e73cb593aae586fa04673068dec6c13bbed409e0
idominguez <israel.dominguez.garcia@gmail.com>
2020-01-02 12:58:40 +0100

    gtm events facade

---
+++ src/Facades/GtmEventsFacade.php
@@ -0,0 +1,11 @@
+<?php namespace Genetsis\Admin\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class GtmEventsFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'GtmEvents';
+    }
+}
