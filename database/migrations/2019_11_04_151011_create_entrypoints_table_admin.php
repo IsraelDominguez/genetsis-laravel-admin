b3654f0dc287938b430da013d9d086da4460133a
idominguez <israel.dominguez.garcia@gmail.com>
2020-06-24 16:55:55 +0200

    change druid app table admin

--- database/migrations/2019_11_04_151011_create_entrypoints_table_admin.php
+++ database/migrations/2019_11_04_151011_create_entrypoints_table_admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEntrypointsTable extends Migration
+class CreateEntrypointsTableAdmin extends Migration
 {
     /**
      * Run the migrations.
