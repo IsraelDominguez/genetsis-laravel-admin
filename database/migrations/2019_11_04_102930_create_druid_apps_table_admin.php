707567ac4960a80f29ceb22243ed8c1b9eb9b07d
idominguez <israel.dominguez.garcia@gmail.com>
2020-06-24 16:42:27 +0200

    change druid app table admin

--- database/migrations/2019_11_04_102930_create_druid_apps_table_admin.php
+++ database/migrations/2019_11_04_102930_create_druid_apps_table_admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDruidAppsTable extends Migration
+class CreateDruidAppsTableAdmin extends Migration
 {
     /**
      * Run the migrations.
